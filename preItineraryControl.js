ff34c12e16f740612f1804dd0b755978c2a73a08
adamZdrojewski <adam.zdrojewski2@gmail.com>
2020-03-09 17:23:04 -0400

    adds itinerary to pretrip page

---
+++ preItineraryControl.js
@@ -0,0 +1,11 @@
+function toFri() {
+	window.location.href = "preItineraryFri.html";
+}
+
+function toSat() {
+	window.location.href = "preItinerarySat.html";
+}
+
+function toSun() {
+	window.location.href = "preItinerarySun.html";
+}
--- pretripScript.js
+++ pretripScript.js
@@ -33,3 +33,14 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+//Sidenav
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
